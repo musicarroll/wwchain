d8006ad88ecc5dd69df450a743c1fa9de07fc137
musicarroll <musicarroll@gmail.com>
2025-07-18 13:53:36 -0600

    Derive PartialEq for Transaction

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Transaction {
     pub sender: String,
     pub recipient: String,
